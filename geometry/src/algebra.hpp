c451882fa43df2810996d57a36a18b1093a7ac7c
Oliver Lee <oliverzlee@gmail.com>
2024-01-06 01:18:42 -0800

    define geometric product for blades
    Change-Id: Ic3b5852852e80237022248f54c2948835afa4949

--- geometry/src/algebra.hpp
+++ geometry/src/algebra.hpp
@@ -31,6 +31,56 @@ struct algebra
   /// Defines a blade for the given algebra.
   ///
   template <std::size_t... Is>
+  struct blade;
+
+private:
+  template <std::size_t... Is>
+  static auto rebind(std::index_sequence<Is...>) -> blade<Is...>;
+
+  template <class Seq>
+  using rebind_t = decltype(rebind(Seq{}));
+
+  template <std::size_t... Is>
+  using reified_blade_t = rebind_t<detail::contract_dimensions_t<
+      detail::contraction_map::projective,
+      detail::sort_dimensions_t<std::index_sequence<Is...>>>>;
+
+  template <std::size_t... Is>
+  static constexpr auto reified_blade_coefficient_v =
+      (detail::sort_dimensions_swap_count_v<std::index_sequence<Is...>> % 2 == 0
+           ? scalar_type{1}
+           : -scalar_type{1}) *
+      scalar_type{detail::contract_dimensions_coefficient_v<
+          detail::contraction_map::projective,
+          detail::sort_dimensions_t<std::index_sequence<Is...>>>};
+
+public:
+  /// unit blade
+  /// @tparam Is dimensions
+  ///
+  /// Obtains the unit blade corresponding to dimensions `Is...` for the given
+  /// algebra. `Is...` will be reified if not provided in canonical form (i.e.
+  /// in non-decreasing order).
+  ///
+  /// ~~~{.cpp}
+  /// static_assert(e<1, 2> == -e<2, 1>);
+  /// ~~~
+  ///
+  /// A unit blade specified with zero dimensions (i.e. `e<>`) is semantically
+  /// equivalent to a scalar.
+  ///
+  /// ~~~{.cpp}
+  /// static_assert(2 == 2*e<>);
+  /// ~~~
+  ///
+  /// Squared basis vectors in a blade (i.e. `e<i, i>`) will contract to `0` for
+  /// `i == 0` and `1` for `i != 0`.
+  ///
+  template <std::size_t... Is>
+  static constexpr auto e =
+      reified_blade_t<Is...>{reified_blade_coefficient_v<Is...>};
+
+  template <std::size_t... Is>
   struct blade
   {
     static_assert(sizeof...(Is) <= N + 1, "number of `Is` exceeds rank");
@@ -138,54 +188,17 @@ struct algebra
       return a * x;
     }
     /// @}
+
+    /// geometric product
+    ///
+    template <std::size_t... Js>
+    [[nodiscard]]
+    friend constexpr auto
+    operator*(blade x, blade<Js...> y)
+    {
+      return x.coefficient * y.coefficient * e<Is..., Js...>;
+    }
   };
-
-private:
-  template <std::size_t... Is>
-  static auto rebind(std::index_sequence<Is...>) -> blade<Is...>;
-
-  template <class Seq>
-  using rebind_t = decltype(rebind(Seq{}));
-
-  template <std::size_t... Is>
-  using reified_blade_t = rebind_t<detail::contract_dimensions_t<
-      detail::contraction_map::projective,
-      detail::sort_dimensions_t<std::index_sequence<Is...>>>>;
-
-  template <std::size_t... Is>
-  static constexpr auto reified_blade_coefficient_v =
-      (detail::sort_dimensions_swap_count_v<std::index_sequence<Is...>> % 2 == 0
-           ? scalar_type{1}
-           : -scalar_type{1}) *
-      scalar_type{detail::contract_dimensions_coefficient_v<
-          detail::contraction_map::projective,
-          detail::sort_dimensions_t<std::index_sequence<Is...>>>};
-
-public:
-  /// unit blade
-  /// @tparam Is dimensions
-  ///
-  /// Obtains the unit blade corresponding to dimensions `Is...` for the given
-  /// algebra. `Is...` will be reified if not provided in canonical form (i.e.
-  /// in non-decreasing order).
-  ///
-  /// ~~~{.cpp}
-  /// static_assert(e<1, 2> == -e<2, 1>);
-  /// ~~~
-  ///
-  /// A unit blade specified with zero dimensions (i.e. `e<>`) is semantically
-  /// equivalent to a scalar.
-  ///
-  /// ~~~{.cpp}
-  /// static_assert(2 == 2*e<>);
-  /// ~~~
-  ///
-  /// Squared basis vectors in a blade (i.e. `e<i, i>`) will contract to `0` for
-  /// `i == 0` and `1` for `i != 0`.
-  ///
-  template <std::size_t... Is>
-  static constexpr auto e =
-      reified_blade_t<Is...>{reified_blade_coefficient_v<Is...>};
 };
 
 }  // namespace geometry
